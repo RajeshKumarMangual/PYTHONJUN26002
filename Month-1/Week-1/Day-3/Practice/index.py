551ff15f1bec4666c6a3d47fff71ee0d53a1f5ac
1Rajesh Kumar Mangual <rajeshkumarmangual@gmail.com>
2026-06-13 17:48:42 +0530

    day 3 practice

---
+++ Month-1/Week-1/Day-3/Practice/index.py
@@ -0,0 +1,64 @@
+# if (False):
+  #  print("This is the first line")
+#a = input("Enter a number: ")
+
+#if (int(a) > 0):
+ #   print("a is greater than 0")
+#else:
+#    print("a is not greater than 0")
+
+
+# battery = int(input("Enter the battery percentage: "))
+# if (battery == 100):
+#     print("Battery is full")
+# elif (battery >= 50):
+#     print("Battery is more than 80%")
+# elif (battery < 20):
+#     print("save your Battery")
+# else:
+#     print("Switch off your phone")
+
+
+
+# email = input("Enter your email: ")
+# password = input("Enter your password: ")
+# if email == "admin@admin.com":
+#      if password == "admin123":
+#        print("Login successful")
+#      else:
+#        print("Incorrect password")
+# else:
+#     print("Not an admin user")
+
+
+
+#LOOPS
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(1, 11):
+#     print(i)
+
+# for i in range(1, 11, 2):
+#     print(i)
+
+
+#WHILE LOOP
+
+# i = 1
+# while i <= 5:
+#     print(i)
+
+#     i += 1
+
+
+# 5 to 0
+# for x in range (5, 0, -1):
+#     print(x)
+
+
+y = 5
+while y >= 0:
+    print(y)
+    y -= 1
---
+++ Month-1/Week-1/Day-5/Practice/index.py
@@ -0,0 +1,44 @@
+# ice_creame=["vanila","chocolate","butterchoch"]
+# print(ice_creame)
+# print(ice_creame[0])
+# # at end
+# ice_creame.append("blueberry")
+# print(ice_creame)
+# # at index
+# ice_creame.insert(1,"blueberry")
+# # remove perticular
+# ice_creame.remove("chocolate")
+# print(ice_creame)
+# # from end
+# ice_creame.pop()
+# print(ice_creame)
+# print(len(ice_creame))
+
+# for item in ice_creame:
+#     print(item)
+
+
+# tuple
+# building=("b1","b2","b3","b3")
+# print(building)
+
+
+# set
+# set={1,4,7,9,0,1,0}
+# print(set)
+# set.add(6)
+# set.remove(4)
+# print(set)
+
+# dict
+# school={"name":"BSpur school","add":"BBSR","building":6}
+# print(school)
+# school["students"]=1000
+# print(school)
+# for key,val in school.items():
+#     print(key,'-',val)
+
+# string methods--
+name="kaml"
+print(name.upper())
+print(name.replace("kaml","kamal"))
